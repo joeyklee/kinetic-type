c89b753f8c62a4f67937e9b3b3ba6a803689e9cc
joeyklee <j@jk-lee.com>
2018-07-10 23:11:20 -0400

    added hello-goodbye sketch - each mover only has 1 attractor

--- app/sketch.js
+++ app/sketch.js
@@ -31,13 +31,13 @@
  * ctrl                : save png
  */
 
-var textTyped = 'A';
+var textTyped = ['Hello', 'Bye'];
+
 
 var font;
-
-var filled = false;
-let path;
 let movers = [];
+let attractors = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noLoop();
@@ -48,70 +48,43 @@ function setup() {
     } else {
       font = f;
 
-      if (textTyped.length > 0) {
-        // get a path from OpenType.js
-        var fontPath = font.getPath(textTyped, 0, 0, 200);
-        // convert it to a g.Path object
-        path = new g.Path(fontPath.commands);
-        // resample it with equidistant points
-        path = g.resampleByLength(path, 6);
-        // path = g.resampleByAmount(path, 500);
-
-        // map mouse axis
-        // var addToAngle = map(mouseX, 0, width, -PI, PI);
-        // var curveHeight = map(mouseY, 0, height, 0.1, 2);
-
-        let counter = 0;
-        path.commands.forEach( (pt, idx) => {
-        	if(pt.type == "M"){
-        		counter++;
-        	}
-        	else{
-        		if(counter){
-        			movers.push(new Mover(pt.x, pt.y, 4, true))	
-        		} else{
-        			movers.push(new Mover(pt.x, pt.y, 4, false))	
-        		}
-
-        		// movers.push(new Mover(pt.x, pt.y, 4, false))
-        	} 
-        	
-        })
-      }
+      // create 
+      movers = createFontPaths(textTyped[0], font, false)
+      attractors = createFontPaths(textTyped[1], font, true)
 
       loop();
     }
   });
 }
 
+
 function draw() {
   if (!font) return;
+  randomSeed(1)
 
   background(255, 255, 255, 20);
-  if (filled) {
-    noStroke();
-    fill(0);
-  } else {
-    noFill();
-    stroke(0);
-    strokeWeight(2);
-  }
 
   // margin border
   translate(300, 300);
 
   movers.forEach( (mover1, idx1) =>{
-  	movers.forEach( (mover2, idx2) => {
-  		if(idx1 !== idx2){
-  				let moverAttractionforce = mover2.attract(mover1)
-	  		  mover1.applyForce(moverAttractionforce)	
-  		}
-  	})
+
+    if(frameCount > 150){
+
+
+    let attractionForce = attractors[int(random(0,attractors.length))].attract(mover1);
+    mover1.applyForce(attractionForce)
+    }
+
+	  // mover1.applyForce(moverAttractionforce)	
   	mover1.update();
   	mover1.display();
   })
   
 }
+
+
+
 
 
 class Mover{
@@ -129,10 +102,25 @@ class Mover{
 		this.location = createVector(this.x, this.y);
 		this.velocity = createVector(0,0);
 		this.acceleration = createVector(0,0);
+		
 	}
 }
 
 Mover.prototype.attract = function( _mover ){
+	let force = p5.Vector.sub(this.location, _mover.location)
+
+	let distance = force.mag();
+	distance = constrain(distance,1.0,2.0);
+	force.normalize();
+
+	let strength = (this.G * this.mass * _mover.mass) / (distance * distance);	
+	force.mult(strength);
+
+  return force;
+
+}
+
+Mover.prototype.repel = function( _mover ){
 	let force = p5.Vector.sub(this.location, _mover.location)
 
 	let distance = force.mag();
@@ -141,32 +129,7 @@ Mover.prototype.attract = function( _mover ){
 
 	let strength = (this.G * this.mass * _mover.mass) / (distance * distance);
 
-
-	if(_mover.attractor == true){
-		
-		force.mult(strength);
-
-	} else{
-		force.mult(0);
-	}
-
-  return force;
-
-}
-
-Mover.prototype.repel = function( _mover ){
-	let force = p5.Vector.sub(_mover.location, createVector(this.x, this.y))
-
-	let distance = force.mag();
-	distance = constrain(distance,100.0,200.0);
-	force.normalize();
-
-	let strength = (this.G * this.mass * _mover.mass) / (distance * distance);
-
-		
 	force.mult(strength);
-
-
   return force;
 
 }
@@ -184,25 +147,18 @@ Mover.prototype.applyForce = function( force ){
 
 Mover.prototype.update = function(){
 	
-	// this.angularVelocity += this.angularAcceleration;
-	// this.angle += this.angularVelocity;
-	// if(this.angularVelocity > 3){
-	// 	this.angularVelocity= 3;
-	// }
-
-	let distorterX = map(mouseX, 0, width, -2, 2)
-	let distorterY = map(mouseY, 0, height, -2, 2)
+	this.angularVelocity += this.angularAcceleration;
+	this.angle += this.angularVelocity;
+	if(this.angularVelocity > 3){
+		this.angularVelocity= 3;
+	}
 
 	if(mouseIsPressed) this.acceleration = createVector(random(-0.25, 0.25), random(-0.25, 0.25))
-	// this.acceleration = createVector(distorterX, distorterY)
 
-
-		this.velocity.add(this.acceleration)
-		this.location.add(this.velocity)
-
-		this.velocity.limit(1)
-
-		this.acceleration.mult(0)	
+	this.velocity.add(this.acceleration)
+	this.location.add(this.velocity)
+	this.velocity.limit(1)
+	this.acceleration.mult(0)	
 	
 }
 
@@ -217,6 +173,31 @@ Mover.prototype.display = function(){
 	line(-this.mass/2, 0, this.mass/2, 0)
 	// line(0, 0, this.mass, 0)
 	pop();
+}
+
+
+
+
+
+/*@@
+@ keys & helpers
+*/
+
+function createFontPaths(textTyped, font, attractorStatus){
+  let output = [];
+  // get a path from OpenType.js
+  var fontPath = font.getPath(textTyped, 0, 0, 200);
+  // convert it to a g.Path object
+  path = new g.Path(fontPath.commands);
+  // resample it with equidistant points
+  path = g.resampleByLength(path, 2);
+  // path = g.resampleByAmount(path, 500);
+
+  path.commands.forEach( (pt, idx) => {
+      output.push(new Mover(pt.x, pt.y, 4, attractorStatus)) 
+  })
+
+  return output;
 }
 
 function keyReleased() {
